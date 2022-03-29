86ca1cf07a653dd72d34599406ce7ca7d53c114c
ollie <olivinanzali07@gmail.com>
2022-03-29 11:50:35 -0400

    updated js

--- Downloads/connect-four/myconnect4.js
+++ Downloads/connect-four/myconnect4.js
@@ -8,7 +8,7 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-const currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -54,12 +54,12 @@ function makeHtmlBoard() {
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
-function findSpotForCol(x) {
+function findSpotForCol(col){
   // TODO: write the real version of this, rather than always returning 0
-  for(let y = HEIGHT - 1; y>= 0; y--) {
+  for(let row = HEIGHT - 1; row>= 0; row--) {
     // checks if cell is empty. 
-    if(!board[y][x]) {
-      return y;
+    if(!board[row][col]) {
+      return row;
     }
   }
   return null;
@@ -67,21 +67,25 @@ function findSpotForCol(x) {
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
-function placeInTable(y, x) {
+function placeInTable(col, row) {
   // TODO: make a div and insert into correct table cell
-  const piece = document.querySelector('div');
-  piece.classList.add('.player-one')
-  piece.style.top = -43 * (y+ 2);
-
-  const circle = document.getElementById(`${y} - ${x}`);
-  piece.append(circle);
+  // const piece = document.querySelector('div');
+  // piece.classList.add('.player-one')
+  // piece.style.top = -43 * (row+ 2);
+  console.log(col, row);
+  const circle = document.getElementById(`${row}-${col}`);
+  if (currPlayer === 1){
+    circle.classList.add('player-one');
+  } else {
+    circle.classList.add('player-two');
+  }
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
-  alert("Game Over!")
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -89,37 +93,44 @@ function endGame(msg) {
 function handleClick(evt) {
   // if what you clicked on does not have "piece" class function ends.
   if(!evt.target.classList.contains("piece")){
-    return;
+    alert("You cannot Click there!");
   }
   // get x from ID of clicked cell
   console.log(evt.target.id);
   console.log(evt.target.id.split("-"));
   //converting it to a number
-  let x = +evt.target.id.split("-")[1];
-  console.log(x);
+  let col = +evt.target.id.split("-")[1];
+  console.log(col);
 
   // get next spot in column (if none, ignore click)
-  const y = findSpotForCol(x);
-  if (y === null) {
+  const row = findSpotForCol(col);
+  if (row === null) {
     return;
   }
-  console.log(y);
+  console.log(row);
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
-  board[x]= currPlayer;
-  placeInTable(y, x);
-  
+  board[row][col]= currPlayer;
+  placeInTable(col, row);
 
-  // check for win
-  if (checkForWin()) {
+   // check for win
+   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
+  
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  if (board.every(row => row.every(cell => cell))) {
+    return endGame('Tie!');
+  }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  if (currPlayer === 1) {
+    currPlayer = 2;
+  } else {
+    currPlayer = 1;
+  }
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -151,7 +162,7 @@ function checkForWin() {
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
-      }
+      } 
     }
   }
 }
